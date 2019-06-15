b353dafaf26ceda1039f4c22ae482eb9d2c31867
Jotham Ritorze <jotham.ritorze@gmail.com>
2019-06-15 15:14:06 +1000

    CameraControl

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour 
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 6, -10);
+
+    void Start()
+    {
+        this.transform.position = target.position + offset;
+        this.transform.LookAt(target.position);
+    }
+
+    void Update()
+    {
+        this.transform.position = target.position + offset;
+    }
+
+}
