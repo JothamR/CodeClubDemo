e323029ae4fa4cf6da89f52d043eac6095c09fdd
Jotham Ritorze <jotham.ritorze@gmail.com>
2019-06-15 14:50:09 +1000

    Helpers

---
+++ Assets/Scripts/Helpers/GamePad.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+public class GamePad : MonoBehaviour
+{
+    ////XBox Controller
+    public static Vector3 Direction = new Vector3(0, 0, 0); //thumbstick (or arrow keys)
+    public static bool ButtonA, ButtonB, ButtonX, ButtonY; //buttons
+
+    //public static Vector3 Aim = new Vector3(0, 0, 0); //right-thumbstick
+    public static bool HasDirection => Direction.sqrMagnitude > 0.01; //ignore centre deadzone
+    public void Update()
+    {
+        GetInput();
+    }
+
+    public static void GetInput()
+    {
+        //Thumbstick (or arrow keys)
+        var h = Input.GetAxis("Horizontal"); //x-axis
+        var v = Input.GetAxis("Vertical");   //z-axis
+        Direction.Set(h, 0f, v);             //Set 'Direction' based on input.
+
+        //Right thumbstick (for aiming)
+        //h = Input.GetAxis("Axis4");
+        //v = Input.GetAxis("Axis5");
+        //Aim.Set(h, 0f, v);
+
+        //Buttons
+        ButtonA = Input.GetButton("Fire1"); //Left Shift
+        ButtonB = Input.GetButton("Fire2"); //Left Alt
+        ButtonX = Input.GetButton("Fire3"); //Left Ctrl
+        ButtonY = Input.GetButton("Jump");  //Space
+    }
+}
---
+++ Assets/Scripts/Helpers/MathHelpers.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class MathHelpers
+{
+    public static Vector3 SetX(this Vector3 vec, float x) { vec.x = x; return vec; } //only change X
+    public static Vector3 SetY(this Vector3 vec, float y) { vec.y = y; return vec; } //only change Y
+    public static Vector3 SetZ(this Vector3 vec, float z) { vec.z = z; return vec; } //only change Z
+
+    public static Vector3 AddX(this Vector3 vec, float x) { vec.x += x; return vec; } //only change X
+    public static Vector3 AddY(this Vector3 vec, float y) { vec.y += y; return vec; } //only change Y
+    public static Vector3 AddZ(this Vector3 vec, float z) { vec.z += z; return vec; } //only change Z
+
+}
