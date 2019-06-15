e23fe50caf11c805453d37521dea4af7f7829ca7
Jotham Ritorze <jotham.ritorze@gmail.com>
2019-06-15 15:12:41 +1000

    RigidBody Look + Move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody _body;
+
     public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = this.transform.position + GamePad.Direction * speed * Time.deltaTime;
+        //Look
+        if (GamePad.HasDirection) transform.forward = GamePad.Direction;
+    }
+
+    void FixedUpdate()
+    {
+        //Move
+        _body.MovePosition(_body.position + GamePad.Direction * speed * Time.fixedDeltaTime);
     }
 }
